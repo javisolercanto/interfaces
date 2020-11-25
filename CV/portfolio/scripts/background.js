24377deb192032dfcc0a6418e5cd234d35491fd8
javisolercanto <jsolercanto11@gmail.com>
2020-11-26 00:53:40 +0100

    removed bg script

--- CV/portfolio/scripts/background.js
+++
@@ -1,8 +0,0 @@
-fillbackground();
-    function fillbackground() {
-        let text = '';
-        for (let i = 0; i < 50; i++) {
-            text += 'Javier Soler Cantó ';
-        }
-        document.getElementById('bg-text').appendChild(document.createTextNode(text))
-    }
